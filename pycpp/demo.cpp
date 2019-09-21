8a551a7e16b736e0e55121e03230fa5cff21e03b
LucasWaelti <lucas.waelti@gmail.com>
2019-09-21 20:18:20 +0200

    Implemented simple dataset generation.

--- pycpp/demo.cpp
+++ pycpp/demo.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
+#include <math.h>
 #include <torch/torch.h>
 
-#define BASICS 
+//#define BASICS 
 #define LEARN
-#define DIM 100 // Dataset size
+#define DIM 10 // Dataset size
+
+
+class Net: torch::nn::Module{
+  public: 
+    Net(){
+      // Constructor - build the network's layers
+      _in = register_module("in",torch::nn::Linear(2,10));
+      _h = register_module("h",torch::nn::Linear(10,5));
+      _out = register_module("out",torch::nn::Linear(5,1));
+    }
+    
+    torch::Tensor forward(torch::Tensor x){
+      // apply ReLU activations and sigmoid for the output
+      x = torch::relu(_in->forward(x));
+      x = torch::relu(_h->forward(x));
+      x = torch::sigmoid(_out->forward(x));
+        
+      // return the output
+      return x;
+    }
+
+  private:
+    torch::nn::Linear _in{nullptr},_h{nullptr},_out{nullptr};
+};
 
 torch::Tensor create2DDataSet(unsigned int dim=DIM){
   return torch::rand({dim,2});
 }
 
+double rule(double x, double y, double r=0.5){
+  // 0 if contained in circle with radius r, 
+  // 1 if outside
+  if(sqrt(pow(x,2) + pow(y,2)) < r){
+    return 0.;
+  }else{
+    return 1.;
+  }
+}
+
 torch::Tensor createGroundTruth(const torch::Tensor& input){
   torch::Tensor out = torch::empty(input.size(0));
   for(int i=0; i<input.size(0); i++){
-    return torch::rand(input.size(0));
+    if(rule(input[i][0].item().toDouble(),
+            input[i][1].item().toDouble()) == 0){
+      out[i] = 0.;
+    }else{
+      out[i] = 1.;
+    }
   }
+  return out;
 }
 
 int main() {
@@ -37,7 +78,14 @@ int main() {
   t = torch::cat({t,t1},0);
   std::cout << t << std::endl << std::endl; 
 
-#elif LEARN
-  // TODO 
+#elif defined(LEARN) 
+  Net model = Net(); 
+
+  torch::Tensor data  = create2DDataSet(DIM);
+  torch::Tensor truth = createGroundTruth(data);
+  std::cout << data  << std::endl;
+  std::cout << truth << std::endl; 
+  
 #endif
+  return EXIT_SUCCESS;
 }
--- pycpp/demo.py
+++ pycpp/demo.py
@@ -1,15 +1,56 @@
-
+import math 
 import torch 
+from torch.nn import functional as F
 
-BASICS = True
-DIM = 4 # Dataset size
+BASICS = False
+LEARN = True 
+DIM = 10 # Dataset size
+
+
+class Net(torch.nn.Module):
+    
+    def __init__(self):
+        super(Net, self).__init__()
+        self._in = torch.nn.Linear(2, 10)
+        self._h = torch.nn.Linear(10, 5)
+        self._out = torch.nn.Linear(5, 1)
+
+    
+    def forward(self, x):
+        # apply ReLU activations and sigmoid for the output
+        x = F.relu(self._in(x))
+        x = F.relu(self._h(x))
+        x = F.sigmoid(self._out(x))
+
+        # return the output
+        return x 
+
+
+
+
+
 
 def create2DDataSet(dim:int):
     return torch.rand([dim,2])
 
 
-def createGroundTruth(input:torch.tensor):
-    out = torch.empty(input.size(0))
+def rule(x,y,r=0.5):
+    # 0 if contained in circle with radius r, 
+    # 1 if outside
+    if math.sqrt(x**2 + y**2) < r:
+        return 0
+    else:
+        return 1 
+
+
+
+def createGroundTruth(inp:torch.tensor):
+    out = torch.empty(inp.size(0))
+    for i,point in enumerate(inp):
+        if rule(point[0],point[1]) == 0:
+            out[i] = 0
+        else:
+            out[i] = 1
     return out 
 
 
@@ -34,12 +75,16 @@ def main():
         print("Concatenate tensors:")
         t1 = torch.tensor([[5,6]])
         
-        
         t = torch.cat([t,t1],0)
         print(t)
 
+    elif LEARN:
+        model = Net()
 
-
+        data  = create2DDataSet(DIM)
+        truth = createGroundTruth(data)
+        print(data)
+        print(truth) 
 
 
 
